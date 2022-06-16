3530e32905759eedb1f52cf62a91de552c574088
ShivamP3 <107635980+ShivamP3@users.noreply.github.com>
2022-06-16 11:57:31 -0400

    Add files via upload

---
+++ school/grade8/science/chapter9/Loading.js
@@ -0,0 +1,13 @@
+var myVar;
+
+function homePage() {
+  location.href = "Basics.html";
+}
+
+window.onload = function () {
+  myVar = setTimeout(homePage, 3000);
+}
+
+function helloWorld() {
+  
+}
---
+++ school/grade8/science/chapter9/script.js
@@ -0,0 +1,56 @@
+$(function(){
+
+    window.sr = ScrollReveal();
+  
+    if ($(window).width() < 768) {
+  
+        if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+            $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+        }
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+    } else {
+        
+        sr.reveal('.js--fadeInLeft', {
+          origin: 'left',
+          distance: '300px',
+            easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+    }
+    
+    sr.reveal('.js--fadeInLeft', {
+          origin: 'left',
+          distance: '300px',
+            easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+  
+  });
+  
+
+function helloWorld() {
+  document.getElementById("name").innerHTML = "Age 13";
+}
